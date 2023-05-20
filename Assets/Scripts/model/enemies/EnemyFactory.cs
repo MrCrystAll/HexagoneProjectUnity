9ca9ee8cad7a01968dfb0b487be1a230b29a789e
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-05-20 22:02:53 +0200

    Adding project settings for build settings

--- Assets/Scripts/model/enemies/EnemyFactory.cs
+++ Assets/Scripts/model/enemies/EnemyFactory.cs
@@ -4,12 +4,12 @@
     {
         public static Enemy CreateLv1Enemy()
         {
-            return new Enemy(50, 10, 5, 50);
+            return new Enemy(5, 10, 5, 50);
         }
         
         public static Enemy CreateLv2Enemy()
         {
-            return new Enemy(20, 40, 10, 100);
+            return new Enemy(2, 40, 10, 100);
         }
         
         public static Enemy CreateLv3Enemy()
